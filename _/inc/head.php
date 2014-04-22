2c9082f31e0a220214226c737c10fc69f50db9f5
Ryan Maskell <ryan@savorweb.com>
2014-04-22 16:58:40 -0700

    nav refactored

--- _/inc/head.php
+++ _/inc/head.php
@@ -18,197 +18,80 @@
 </head>
 <body>
 	<header class="pagehead pullwidth">
-<section class="row">
-		<div class="navtop">
-			<img class="is-mobile white-logo" src="_/img/logotype.png" alt="">
+		<section class="row">
+			<div class="navtop">
+				<img class="white-logo" src="_/img/logotype.png" alt="">
 
-			<button type="button" role="button" id="nav-toggle" class="lines-button x2"><span class="lines"></span></button>
-		</div>
+				<button type="button" role="button" id="nav-toggle" class="lines-button x2"><span class="lines"></span></button>
+			</div>
 
-		<nav class="nav-collapse">
-			<ul>
-				<li><a href="/newjvw/">Home</a></li>
-				<li><a href="#contact">Contact</a></li>
-				<li><a href="/about">About</a></li>
-				<li><a href="/attorneys">Attorneys</a></li>
-			</ul>
-			<ul class="phonenumbers">
-				<li>Local callers <a href="tel:6676670683">(208) 667-0683</a></li>
-				<li>Toll free line <a href="tel:8886670683">(888) 667-0683</a></li>
-			</ul>
-		</nav>
-		<!-- <a class="logo" href="/"><img src="_/img/logo.png" alt=""> --><!-- <object type="image/svg+xml" data="assets/img/logo.svg" alt="Magnuson Hotels Logo"></object> --><!-- </a> -->
-		
-<!-- 		<section class="logos">
-			<div class="logotype"><img src="http://www.placehold.it/400x100" alt=""></div>
+			<nav class="nav-collapse">
+				<ul>
+					<li><a href="/newjvw/">Home</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li><a href="/about">About</a></li>
+					<li><a href="/attorneys">Attorneys</a></li>
+				</ul>
+				<ul class="phonenumbers">
+					<li><a href="tel:6676670683"><span>Local callers</span> (208) 667-0683</a></li>
+					<li><a href="tel:8886670683"><span>Toll free line</span> (888) 667-0683</a></li>
+				</ul>
+			</nav>
+			<!-- <a class="logo" href="/"><img src="_/img/logo.png" alt=""> --><!-- <object type="image/svg+xml" data="assets/img/logo.svg" alt="Magnuson Hotels Logo"></object> --><!-- </a> -->
+
+		<section class="logos">
+			<div class="logotype"><img src="_/img/logotype-dark.png" alt=""></div>
+			<div class="slogan"><img src="_/img/slogan.png" alt=""></div>
 			<div class="logo"><img src="_/img/logo.png" alt=""></div>
-			<div class="slogan"><img src="http://www.placehold.it/400x100" alt=""></div>
-		</section> -->
-</section>
-		<div class="fullwidthimg"></div>
-	</header>
+		</section>
+	</section>
+	<div class="fullwidthimg"></div>
+</header>
 
 <section class="row">
-		<button type="button" role="button" id="area-toggle">
-			Practice Areas
-		</button>
-			<ul class="area-collapse responsive-accordion responsive-accordion-default bm-larger">
-				<li>
-					<div class="responsive-accordion-head">Injuries<i class="fa fa-chevron-down responsive-accordion-plus fa-fw"></i><i class="fa fa-chevron-up responsive-accordion-minus fa-fw"></i></div>
-					<div class="responsive-accordion-panel">
-						<ul>
-							<li><a href="/accidents">Auto Accidents</a></li>
-							<li><a href="/drugs">Dangerous Drugs</a></li>
-							<li><a href="/products">Dangerous Products</a></li>
-							<li><a href="/malpractice">Medical Malpractice</a></li>
-							<li><a href="/compensation">Workers Compensation</a></li>
-						</ul>
-					</div>
-				</li>
-				<li>
-					<div class="responsive-accordion-head">Personal<i class="fa fa-chevron-down responsive-accordion-plus fa-fw"></i><i class="fa fa-chevron-up responsive-accordion-minus fa-fw"></i></div>
-					<div class="responsive-accordion-panel">
-						<ul>
-							<li><a href="/bankruptcy">Bankruptcy</a></li>
-							<li><a href="/defense">Criminal Defense</a></li>
-							<li><a href="/family">Family Law</a></li>
-							<li><a href="/planning">Wills, Estate Planning, and Probate</a></li>
-						</ul>
-					</div>
-				</li>
-				<li>
-					<div class="responsive-accordion-head">Business<i class="fa fa-chevron-down responsive-accordion-plus fa-fw"></i><i class="fa fa-chevron-up responsive-accordion-minus fa-fw"></i></div>
-					<div class="responsive-accordion-panel">
-						<ul>
-							<li><a href="/business">Business and Corporate Law</a></li>
-							<li><a href="/employment">Employment Law</a></li>
-							<li><a href="/official">Municipal and Administrative Law</a></li>
-							<li><a href="/reservation">Patents, Trademarks and Copyrights</a></li>
-						</ul>
-					</div>
-				</li>
-				<li>
-					<a href="/real-estate"><div href="/real-estate" class="responsive-accordion-head">Real Estate</div></a>
-				</li>
-				<li>
-					<a href="/sexual-abuse"><div href="/sexual-abuse" class="responsive-accordion-head">Sexual Abuse</div></a>
-				</li>
-			</ul>
+	<button type="button" role="button" id="area-toggle">
+		Practice Areas
+	</button>
+	<ul class="area-collapse responsive-accordion responsive-accordion-default bm-larger">
+		<li>
+			<div class="responsive-accordion-head">Injuries<i class="fa fa-chevron-down responsive-accordion-plus fa-fw"></i><i class="fa fa-chevron-up responsive-accordion-minus fa-fw"></i></div>
+			<div class="responsive-accordion-panel">
+				<ul>
+					<li><a href="/accidents">Auto Accidents</a></li>
+					<li><a href="/drugs">Dangerous Drugs</a></li>
+					<li><a href="/products">Dangerous Products</a></li>
+					<li><a href="/malpractice">Medical Malpractice</a></li>
+					<li><a href="/compensation">Workers Compensation</a></li>
+				</ul>
+			</div>
+		</li>
+		<li>
+			<div class="responsive-accordion-head">Personal<i class="fa fa-chevron-down responsive-accordion-plus fa-fw"></i><i class="fa fa-chevron-up responsive-accordion-minus fa-fw"></i></div>
+			<div class="responsive-accordion-panel">
+				<ul>
+					<li><a href="/bankruptcy">Bankruptcy</a></li>
+					<li><a href="/defense">Criminal Defense</a></li>
+					<li><a href="/family">Family Law</a></li>
+					<li><a href="/planning">Wills, Estate Planning, and Probate</a></li>
+				</ul>
+			</div>
+		</li>
+		<li>
+			<div class="responsive-accordion-head">Business<i class="fa fa-chevron-down responsive-accordion-plus fa-fw"></i><i class="fa fa-chevron-up responsive-accordion-minus fa-fw"></i></div>
+			<div class="responsive-accordion-panel">
+				<ul>
+					<li><a href="/business">Business and Corporate Law</a></li>
+					<li><a href="/employment">Employment Law</a></li>
+					<li><a href="/official">Municipal and Administrative Law</a></li>
+					<li><a href="/reservation">Patents, Trademarks and Copyrights</a></li>
+				</ul>
+			</div>
+		</li>
+		<li>
+			<a href="/real-estate"><div href="/real-estate" class="responsive-accordion-head">Real Estate</div></a>
+		</li>
+		<li>
+			<a href="/sexual-abuse"><div href="/sexual-abuse" class="responsive-accordion-head">Sexual Abuse</div></a>
+		</li>
+	</ul>
 </section>
-<!-- <div class="panel-group" id="accordion">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-					Injuries
-				</a>
-			</h4>
-		</div>
-		<div id="collapseOne" class="panel-collapse collapse">
-			<div class="panel-body">
-				<ul>
-					<li><a href="/accidents">Auto Accidents</a></li>
-					<li><a href="/drugs">Dangerous Drugs</a></li>
-					<li><a href="/products">Dangerous Products</a></li>
-					<li><a href="/malpractice">Medical Malpractice</a></li>
-					<li><a href="/compensation">Workers Compensation</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-					Personal
-				</a>
-			</h4>
-		</div>
-		<div id="collapseTwo" class="panel-collapse collapse">
-			<div class="panel-body">
-				<ul>
-					<li><a href="/bankruptcy">Bankruptcy</a></li>
-					<li><a href="/defense">Criminal Defense</a></li>
-					<li><a href="/family">Family Law</a></li>
-					<li><a href="/planning">Wills, Estate Planning, and Probate</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-					Business
-				</a>
-			</h4>
-		</div>
-		<div id="collapseThree" class="panel-collapse collapse">
-			<div class="panel-body">
-				<ul>
-					<li><a href="/business">Business and Corporate Law</a></li>
-					<li><a href="/employment">Employment Law</a></li>
-					<li><a href="/official">Municipal and Administrative Law</a></li>
-					<li><a href="/reservation">Patents, Trademarks and Copyrights</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a href="/real-estate">
-					Real Estate
-				</a>
-			</h4>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a href="/sexual-abuse">
-					Sexual Abuse
-				</a>
-			</h4>
-		</div>
-	</div>
-</div> -->
-
-<!-- 	<nav class="main-nav">
-		<ul>
-			<li>
-				<a class="dd" href="/injuries">Injuries</a>
-				<ul>
-					<li><a href="/accidents">Auto Accidents</a></li>
-					<li><a href="/drugs">Dangerous Drugs</a></li>
-					<li><a href="/products">Dangerous Products</a></li>
-					<li><a href="/malpractice">Medical Malpractice</a></li>
-					<li><a href="/compensation">Workers Compensation</a></li>
-				</ul>
-			</li>
-			<li>
-				<a class="dd" href="/personal">Personal</a>
-				<ul>
-					<li><a href="/bankruptcy">Bankruptcy</a></li>
-					<li><a href="/defense">Criminal Defense</a></li>
-					<li><a href="/family">Family Law</a></li>
-					<li><a href="/planning">Wills, Estate Planning, and Probate</a></li>
-				</ul>
-			</li>
-			<li>
-				<a class="dd" href="/business">Business</a>
-				<ul>
-					<li><a href="/business">Business and Corporate Law</a></li>
-					<li><a href="/employment">Employment Law</a></li>
-					<li><a href="/official">Municipal and Administrative Law</a></li>
-					<li><a href="/reservation">Patents, Trademarks and Copyrights</a></li>
-				</ul>
-			</li>
-			<li>
-				<a href="/real-estate">Real Estate</a>
-			</li>
-			<li>
-				<a href="/sexual-abuse">Sexual Abuse</a>
-			</li>
-		</ul>
-	</nav> -->
