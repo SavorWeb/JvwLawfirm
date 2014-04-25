d832f3acff0c38a54aa0e519c96464de5d43e5e8
Ryan Maskell <ryan@savorweb.com>
2014-04-25 14:39:43 -0700

    color change / js pruning

--- contact-submit.php
+++ contact-submit.php
@@ -3,51 +3,73 @@ error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
 //If the form is submitted
 if(isset($_POST['submitted'])) {
-	
-	// require a name from user
-	if(trim($_POST['contactName']) === '') {
-		$nameError =  'Forgot your name!'; 
-		$hasError = true;
-	} else {
-		$name = trim($_POST['contactName']);
-	}
+  
+  // require a name from user
+  if(trim($_POST['contactName']) === '') {
+    $nameError =  'Forgot your name!'; 
+    $hasError = true;
+  } else {
+    $name = trim($_POST['contactName']);
+  }
+  
+  // need valid email
+  if(trim($_POST['email']) === '')  {
+    $emailError = 'Forgot to enter in your e-mail address.';
+    $hasError = true;
+  } else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
+    $emailError = 'You entered an invalid email address.';
+    $hasError = true;
+  } else {
+    $email = trim($_POST['email']);
+  }
 
-	// need valid email
-	if(trim($_POST['email']) === '')  {
-		$emailError = 'Forgot to enter in your e-mail address.';
-		$hasError = true;
-	} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
-		$emailError = 'You entered an invalid email address.';
-		$hasError = true;
-	} else {
-		$email = trim($_POST['email']);
-	}
-		
-	// we need at least some content
-	if(trim($_POST['comments']) === '') {
-		$commentError = 'You forgot to enter a message!';
-		$hasError = true;
-	} else {
-		if(function_exists('stripslashes')) {
-			$comments = stripslashes(trim($_POST['comments']));
-		} else {
-			$comments = trim($_POST['comments']);
-		}
-	}
-		
-	// upon no failure errors let's email now!
-	if(!isset($hasError)) {
-		
-		$emailTo = 'ryan@savorweb.com';
-		$subject = 'Mail from '.$name;
-		$sendCopy = trim($_POST['sendCopy']);
-		$body = "Name: $name \n\nEmail: $email \n\nMessage: $comments";
-		$headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+  // need valid phone
+  // if(trim($_POST['phone']) === '')  {
+  //   $phoneError = 'Forgot to enter in your phone number.';
+  //   $hasError = true;
+  // } else if (!preg_match("/^([1]-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$/i", trim($_POST['phone']))) {
+  //   $phoneError = 'You entered an invalid phone number.';
+  //   $hasError = true;
+  // } else {
+  //   $phone = trim($_POST['phone']);
+  // }
+  
+  // This WORKS
+  $phone = trim($_POST['phone']);
 
-		mail($emailTo, $subject, $body, $headers);
+  // validate a phone number
+  // if( !preg_match("/^([1]-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$/i", trim($_POST['email'])) ) { 
+  //     $phoneError = 'Please enter a valid phone number.';
+  //     $hasError = true;
+  // } else {
+  //   $phone = trim($_POST['phone']);
+  // }
+    
+  // we need at least some content
+  if(trim($_POST['comments']) === '') {
+    $commentError = 'You forgot to enter a message!';
+    $hasError = true;
+  } else {
+    if(function_exists('stripslashes')) {
+      $comments = stripslashes(trim($_POST['comments']));
+    } else {
+      $comments = trim($_POST['comments']);
+    }
+  }
+    
+  // upon no failure errors let's email now!
+  if(!isset($hasError)) {
+    
+    $emailTo = 'ryan@savorweb.com';
+    $subject = 'Website contact from '.$name;
+    $sendCopy = trim($_POST['sendCopy']);
+    $body = "Name: $name \n\nEmail: $email \n\nPhone: $phone \n\nRequest: $comments";
+    $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+    mail($emailTo, $subject, $body, $headers);
         
         // set our boolean completion value to TRUE
-		$emailSent = true;
-	}
+    $emailSent = true;
+  }
 }
 ?>
