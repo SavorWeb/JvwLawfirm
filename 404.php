0146a8c686588157d75cd57753f469ecee7754ad
Ryan Maskell <ryan@savorweb.com>
2014-04-25 15:11:34 -0700

    contactus email

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 
 <section id="simple-page" class="row is-typeset editable">
 	<h1>Page not found</h1>
-	<p>It seems the page you were looking for isn't here... If you think it should be, <a href="mailto:ryan@savorweb.com?Subject=404%20page">let us know</a>. Otherwise, try going back to the <a href="/newjvw/">home page</a>.</p>
+	<p>It seems the page you were looking for isn't here... If you think it should be, <a href="mailto:contactus@jvwlaw.net?Subject=404%20page">let us know</a>. Otherwise, try going back to the <a href="/newjvw/">home page</a>.</p>
 </section>
 
 <?php include("_/inc/foot.php"); ?>
